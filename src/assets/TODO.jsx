9422f3a483b01a5c1c7e914e383caddb68b00ff5
dollyllama <edolly2@protonmail.com>
2022-09-16 20:46:30 -0500

    Contact Page

---
+++ src/assets/TODO.jsx
@@ -0,0 +1,5 @@
+/**
+ * TODO: Round top nav corners and keep the bottom left corner unrounded when menu open;
+ * TODO: Make sure mobile menu closes if its open when the screen gets larger;
+ *
+ **/
